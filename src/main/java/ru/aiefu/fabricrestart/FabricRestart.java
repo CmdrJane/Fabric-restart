641bb4e44bd9c621188d69e24c73028e7d549a74
CmdrJane <commanderjanedoe@gmail.com>
2021-09-13 17:08:56 +0300

    -

--- src/main/java/ru/aiefu/fabricrestart/FabricRestart.java
+++ src/main/java/ru/aiefu/fabricrestart/FabricRestart.java
@@ -7,10 +7,11 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Util;
-import ru.aiefu.fabricrestart.commands.RestartCommand;
+import ru.aiefu.fabricrestart.commands.FRCommands;
 
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
-import java.util.Locale;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadFactory;
@@ -18,6 +19,7 @@ import java.util.concurrent.TimeUnit;
 
 public class FabricRestart implements DedicatedServerModInitializer {
 	public static boolean enableRestartScript;
+	public static boolean disableAutoRestart = false;
 	public static String pathToScript;
 	public static long RESTART_TIME;
 	public static long FIRST_MESSAGE_TIME;
@@ -27,16 +29,17 @@ public class FabricRestart implements DedicatedServerModInitializer {
 	public static long COUNTDOWN_TIME;
 	public static String COUNTDOWN_MESSAGE;
 	public static String DISCONNECT_MESSAGE;
-	private static boolean firstPrint = false;
-	private static boolean secondPrint = false;
-	private static volatile int timer = 0;
-	private static volatile int timer2 = 15;
+	public static boolean firstPrint = false;
+	public static boolean secondPrint = false;
+	public static volatile int timer = 0;
+	public static volatile int timer2 = 15;
 	@Override
 	public void onInitializeServer() {
 		Thread test = new Thread(() -> {
 			if(enableRestartScript){
 				String osName = System.getProperty("os.name").toLowerCase();
 				try {
+					new ProcessBuilder(FabricRestart.pathToScript).start();
 					if(osName.startsWith("windows")) {
 						new ProcessBuilder("cmd.exe", "/c", "start call " + FabricRestart.pathToScript).start();
 					} else new ProcessBuilder(FabricRestart.pathToScript).start();
@@ -67,38 +70,38 @@ public class FabricRestart implements DedicatedServerModInitializer {
 			COUNTDOWN_TIME = restart - 16000;
 		});
 		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
-			ThreadFactory threadFactory = new ThreadFactoryBuilder()
-					.setNameFormat("Restart-handler-%d")
-					.setDaemon(true)
-					.build();
-			ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor(threadFactory);
-			executor.scheduleAtFixedRate(() -> {
-				long time = System.currentTimeMillis();
-				if(time > RESTART_TIME){
-					server.execute(() -> {
-						server.getPlayerManager().getPlayerList().forEach(playerEntity -> playerEntity.networkHandler.disconnect(new LiteralText(DISCONNECT_MESSAGE)));
-						server.stop(false);
-					});
-				}
-				else if(!firstPrint && time > FIRST_MESSAGE_TIME){
-					firstPrint = true;
-					server.getPlayerManager().getPlayerList().forEach(playerEntity -> playerEntity.sendSystemMessage(new LiteralText(FIRST_MESSAGE).formatted(Formatting.RED), Util.NIL_UUID));
-				}
-				else if(!secondPrint && time > SECOND_MESSAGE_TIME){
-					secondPrint = true;
-					server.getPlayerManager().getPlayerList().forEach(playerEntity -> playerEntity.sendSystemMessage(new LiteralText(SECOND_MESSAGE).formatted(Formatting.RED), Util.NIL_UUID));
-				}
-				if(time > COUNTDOWN_TIME){
-					++timer;
-					if(timer >= 2){
-						timer = 0;
-						server.getPlayerManager().getPlayerList().forEach(playerEntity -> playerEntity
-								.sendSystemMessage(new LiteralText(String.format(COUNTDOWN_MESSAGE, timer2)).formatted(Formatting.RED), Util.NIL_UUID));
-						--timer2;
+			if(!disableAutoRestart) {
+				ThreadFactory threadFactory = new ThreadFactoryBuilder()
+						.setNameFormat("Restart-handler-%d")
+						.setDaemon(true)
+						.build();
+				ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor(threadFactory);
+				executor.scheduleAtFixedRate(() -> {
+					long time = System.currentTimeMillis();
+					if (time > RESTART_TIME) {
+						server.execute(() -> {
+							server.getPlayerManager().getPlayerList().forEach(playerEntity -> playerEntity.networkHandler.disconnect(new LiteralText(DISCONNECT_MESSAGE)));
+							server.stop(false);
+						});
+					} else if (!firstPrint && time > FIRST_MESSAGE_TIME) {
+						firstPrint = true;
+						server.getPlayerManager().getPlayerList().forEach(playerEntity -> playerEntity.sendSystemMessage(new LiteralText(FIRST_MESSAGE).formatted(Formatting.RED), Util.NIL_UUID));
+					} else if (!secondPrint && time > SECOND_MESSAGE_TIME) {
+						secondPrint = true;
+						server.getPlayerManager().getPlayerList().forEach(playerEntity -> playerEntity.sendSystemMessage(new LiteralText(SECOND_MESSAGE).formatted(Formatting.RED), Util.NIL_UUID));
 					}
-				}
-			}, 0, 500, TimeUnit.MILLISECONDS);
+					if (time > COUNTDOWN_TIME) {
+						++timer;
+						if (timer >= 2) {
+							timer = 0;
+							server.getPlayerManager().getPlayerList().forEach(playerEntity -> playerEntity
+									.sendSystemMessage(new LiteralText(String.format(COUNTDOWN_MESSAGE, timer2)).formatted(Formatting.RED), Util.NIL_UUID));
+							--timer2;
+						}
+					}
+				}, 0, 500, TimeUnit.MILLISECONDS);
+			}
 		});
-		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> RestartCommand.register(dispatcher));
+		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> FRCommands.register(dispatcher));
 	}
 }
---
+++ src/main/java/ru/aiefu/fabricrestart/commands/FRCommands.java
@@ -0,0 +1,56 @@
+package ru.aiefu.fabricrestart.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.Util;
+import ru.aiefu.fabricrestart.FabricRestart;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class FRCommands {
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher){
+        dispatcher.register(CommandManager.literal("restart").requires(source -> source.hasPermissionLevel(4)).executes(context ->
+                execute(context.getSource())).then(CommandManager.literal("delay")
+                .then(CommandManager.argument("time", IntegerArgumentType.integer()).executes(context ->
+                        delayRestart(context.getSource(),IntegerArgumentType.getInteger(context, "time"))))));
+        dispatcher.register(CommandManager.literal("restart-when").executes(context -> getTimeUntilRestart(context.getSource())));
+    }
+
+    private static int execute(ServerCommandSource source){
+        source.getServer().stop(false);
+        return 0;
+    }
+
+    private static int delayRestart(ServerCommandSource source, int minutes){
+        long delaytime = (long) minutes * 60 * 1000;
+        FabricRestart.RESTART_TIME += delaytime;
+        FabricRestart.COUNTDOWN_TIME += delaytime;
+        FabricRestart.FIRST_MESSAGE_TIME += delaytime;
+        FabricRestart.SECOND_MESSAGE_TIME += delaytime;
+        if(delaytime >= 16000){
+            FabricRestart.timer = 0;
+            FabricRestart.timer2 = 15;
+        }
+        if(delaytime >= 60000){
+            FabricRestart.secondPrint = false;
+        }
+        if(delaytime >= 300000){
+            FabricRestart.firstPrint = false;
+        }
+        source.getServer().getPlayerManager().getPlayerList().forEach(player ->
+                player.sendSystemMessage(new LiteralText("Restart has been delayed by " + minutes + " minutes"), Util.NIL_UUID));
+        return 0;
+    }
+
+    private static int getTimeUntilRestart(ServerCommandSource source){
+        source.sendFeedback(new LiteralText("Restart time: " +
+                LocalDateTime.ofEpochSecond(FabricRestart.RESTART_TIME / 1000,0, OffsetDateTime.
+                        now().getOffset()).format(DateTimeFormatter.ofPattern("HH:mm"))), false);
+        return 0;
+    }
+}
